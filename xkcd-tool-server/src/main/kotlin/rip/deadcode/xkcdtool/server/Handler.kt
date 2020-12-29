f4a0b94bd194bf843a186a74f71ce119ed667a48
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2020-12-29 20:37:43 +0900

    wip

--- xkcd-tool-server/src/main/kotlin/rip/deadcode/xkcdtool/server/Handler.kt
+++ xkcd-tool-server/src/main/kotlin/rip/deadcode/xkcdtool/server/Handler.kt
@@ -1,13 +1,19 @@
 package rip.deadcode.xkcdtool.server
 
+import com.google.common.net.MediaType
+import jakarta.servlet.ServletRequest
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.handler.AbstractHandler
+import rip.deadcode.xkcdtool.core.OutputType
 import rip.deadcode.xkcdtool.core.OutputType.EXPLAIN
+import rip.deadcode.xkcdtool.core.OutputType.IMAGE
+import rip.deadcode.xkcdtool.core.OutputType.JSON
 import rip.deadcode.xkcdtool.core.OutputType.NORMAL
 import rip.deadcode.xkcdtool.core.askUrl
 import rip.deadcode.xkcdtool.core.regularize
+import java.util.*
 
 
 class Handler : AbstractHandler() {
@@ -19,11 +25,10 @@ class Handler : AbstractHandler() {
         response: HttpServletResponse
     ) {
 
-        val isExplain = baseRequest.getParameter("explain") != null || baseRequest.getParameter("e") != null
         // Drop `/` of the root. Other slashes are preserved as are
         val nameQuery = regularize(baseRequest.requestURI.drop(1))
 
-        val mode = if (isExplain) EXPLAIN else NORMAL
+        val mode = guessOutputType(baseRequest).orElse(IMAGE)
 
         val redirectUrl = askUrl(nameQuery, mode)
         if (redirectUrl.isPresent) {
@@ -36,4 +41,49 @@ class Handler : AbstractHandler() {
 
         baseRequest.isHandled = true
     }
+}
+
+fun guessOutputType(request: Request): Optional<OutputType> {
+    val isExplainFlagged = request.hasTruthyParam("explain") || request.hasTruthyParam("e")
+    if (isExplainFlagged) {
+        return Optional.of(EXPLAIN)
+    }
+
+    return getOutputTypeFromStr(request.getParameter("type"))
+        .or { getOutputTypeByHeader(request.getHeader("Accept")) }
+}
+
+fun ServletRequest.hasTruthyParam(name: String): Boolean {
+    val param = this.getParameter(name)?.toLowerCase()
+    return param == "" || param == "true" || param == "yes" || param == "y" || param == "1"
+}
+
+fun getOutputTypeFromStr(str: String?): Optional<OutputType> {
+    return Optional.ofNullable(
+        when (str?.toLowerCase()) {
+            "image" -> IMAGE
+            "html" -> NORMAL
+            "json" -> JSON
+            "explain" -> EXPLAIN
+            else -> null
+        }
+    )
+}
+
+@Suppress("UnstableApiUsage")
+fun getOutputTypeByHeader(accept: String): Optional<OutputType> {
+    val mime = try {
+        MediaType.parse(accept)
+    } catch (e: IllegalArgumentException) {
+        return Optional.empty()
+    }
+
+    return Optional.ofNullable(
+        when {
+            mime.type() == "image" -> IMAGE
+            mime.type() == "text" && mime.subtype() == "html" -> NORMAL
+            mime.type() == "application" && mime.subtype() == "json" -> JSON
+            else -> null
+        }
+    )
 }
